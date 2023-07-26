2868d85fb3c96a73bb71dbfc22e51decd63cc270
Buse Karabıyık <karabiyikbuse04@gmail.com>
2023-07-26 19:41:35 +0300

    - got rid of some unused extensions - got track of number of hints left

--- Hangman/Controller/MainViewController.swift
+++ Hangman/Controller/MainViewController.swift
@@ -20,7 +20,8 @@ class MainViewController: UIViewController {
     private var countForNextQuestion = 0
     private var indices: [Int] = []
     private var letterIndexCount = 0
-    var array = Array<Int>()
+    private var array = Array<Int>()
+    private var numberOfHints = 3
     
     
     override func viewDidLoad() {
@@ -31,16 +32,13 @@ class MainViewController: UIViewController {
     }
     
     @IBAction func hintButtonPressed(_ sender: UIButton) {
-        print(countForNextQuestion)
-        print(letterIndexCount)
-        print(array)
-        
+        numberOfHints -= 1
+        hintsLeft.text = String(numberOfHints)
         for x in 0..<indices.count {
             array = array.filter({$0 != indices[x]})
             print(array)
         }
         let randomNumber = array.randomElement()
-//                countForNextQuestion += 1
         updatedWord[randomNumber!] = words[0][randomNumber!]
         indices.append(randomNumber!)
         wordLabel.text = String(updatedWord)
@@ -55,7 +53,6 @@ class MainViewController: UIViewController {
         
         for x in 0..<words[0].count {
             if String(words[0][x]) == (sender.titleLabel?.text)! {
-//                countForNextQuestion += 1
                 updatedWord[x] = words[0][x]
                 indices.append(x)
             }
@@ -75,11 +72,6 @@ class MainViewController: UIViewController {
         
         wordLabel.text = String(updatedWord)
         
-//        if countForNextQuestion == words[0].count {
-//            countForNextQuestion = 0
-//            wordLabel.text = ""
-//            wordManager.fetchData()
-//        }
     }
     
     
@@ -124,16 +116,5 @@ extension StringProtocol {
     subscript(range: PartialRangeThrough<Int>) -> SubSequence { self[...index(startIndex, offsetBy: range.upperBound)] }
     subscript(range: PartialRangeUpTo<Int>) -> SubSequence { self[..<index(startIndex, offsetBy: range.upperBound)] }
 }
-extension String {
-    func numberOfOccurrencesOf(string: String) -> Int {
-        return self.components(separatedBy:string).count - 1
-    }
-}
-extension ClosedRange where Element: Hashable {
-    func random(without excluded:[Element]) -> Element {
-        let valid = Set(self).subtracting(Set(excluded))
-        let random = Int(arc4random_uniform(UInt32(valid.count)))
-        return Array(valid)[random]
-    }
-}
+
 
